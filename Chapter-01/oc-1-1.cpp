10f26699aeca2cbd2b801f022e5d4dd30aa0ae2e
8dgkim <0dgkim@gmail.com>
2020-09-27 16:23:38 +0900

    chapter-01

---
+++ Chapter-01/oc-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int sum;
+
+    sum a + b;
+    std::cout << sum;
+
+}
---
+++ Chapter-01/pr-1-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int num1 = 1;
+    int num2 = 10;
+    int sum;
+
+    sum = (num1 + num2) * (num2 / 2);
+
+    std::cout << sum;
+
+    return 0;
+}
